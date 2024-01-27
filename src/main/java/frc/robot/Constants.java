0ff822aed862f82ba7f241fdae815c550f4f5439
MirandaNg13 <mirandang13@gmail.com>
2024-01-27 10:09:46 -0800

    Numbers Update Swerve

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -20,18 +22,18 @@ public final class Constants {
   }
   
   public static class SwerveConstants{
-    public static final double kWheelRadius = 0.05845; //Need to update
-    public static final int kAngleEncoderResolution = 400; //see https://www.andymark.com/products/hall-effect-two-channel-encoder
+    //public static final double kWheelRadius = 0.05845; //Need to update
+    public static final int kAngleEncoderResolution = 360; //see https://www.andymark.com/products/hall-effect-two-channel-encoder
     public static final int kDriveEncoderResolution = 4096; // neo brushless settings in rev can id setting
   
     public static final double kModuleMaxAngularVelocity = DriveConstants.kMaxAngularSpeed;
     public static final double kModuleMaxAngularAcceleration =
         2 * Math.PI; // radians per second squared
         
-    public static final double kWheelDiameterMeters = kWheelRadius * 2;
-    public static final int kDrivingMotorPinionTeeth = 14;
-    //public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
-    public static final double kDrivingMotorReduction = 2;
+    public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
+    public static final int kDrivingMotorPinionTeeth = 12;
+    public static final double kDrivingMotorReduction = (40 * 20) / (kDrivingMotorPinionTeeth * 40);
+    //public static final double kDrivingMotorReduction = 2;
     public static final double kDrivingEncoderPositionFactor = (kWheelDiameterMeters * Math.PI)
         / kDrivingMotorReduction; // meters
     public static final double kDrivingEncoderVelocityFactor = ((kWheelDiameterMeters * Math.PI)
